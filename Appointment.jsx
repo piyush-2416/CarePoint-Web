18d6916ab470a446bf62d24b3187bcd06e49d9f9
piyush verma <www.pankajverma.981139@gmail.com>
2026-06-29 23:08:06 +0530

    Create Appointment component with Navbar and some api's for goverment schemes

---
+++ Appointment.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
+
+function Appointment() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Appointment Page</h1>
+      
+    </div>
+  )
+}
+
+export default Appointment
