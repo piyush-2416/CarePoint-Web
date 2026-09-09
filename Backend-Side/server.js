00a233c0119f1db421e7ce06fc50548a05c5fe2d
piyush verma <www.pankajverma.981139@gmail.com>
2026-09-09 23:15:04 +0530

    Replace MongoDB URI with placeholder
    Updated MongoDB URI placeholder in server.js for security.

--- Backend-Side/server.js
+++ Backend-Side/server.js
@@ -2,7 +2,7 @@
 // import { MongoClient } from "mongodb";
 // import dotenv from "dotenv";
 // import cors from "cors";
-// const MONGODB_URI="mongodb+srv://wwwpiyushverma2401_db_user:N3hTB85tx.xnStG@piyush2416.sp9zipj.mongodb.net/?appName=Piyush2416"
+// const MONGODB_URI="Put your database url⭐⭐⭐⭐"
 // const Database="Products"
 // const collection="Products-items"
 
@@ -57,7 +57,7 @@ import dotenv from "dotenv";
 import cors from "cors";
     import { ObjectId } from "mongodb"; // top pe
 
-const MONGODB_URI = "mongodb+srv://wwwpiyushverma2401_db_user:N3hTB85tx.xnStG@piyush2416.sp9zipj.mongodb.net/?appName=Piyush2416"; // apna URI daalo
+const MONGODB_URI = "Put your db url⭐⭐⭐⭐☘️"; // apna URI daalo
 const Database = "hospital-data";
 const collection = "Doctors-data";
 const collection2="Medicine-data";
@@ -211,7 +211,7 @@ app.listen(PORT, () => {
 
 // const MONGODB_URI =
 //   process.env.MONGODB_URI ||
-//   "mongodb+srv://wwwpiyushverma2401_db_user:N3hTB85tx.xnStG@piyush2416.sp9zipj.mongodb.net/?appName=Piyush2416";
+//   "put your db  url";
 // const DATABASE_NAME = "Food-data";
 // const COLLECTION_NAME = "Food-items";
 
